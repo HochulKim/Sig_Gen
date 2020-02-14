768549c43f33c25a97c109f5f9b4e712b1216860
HochulKIm <charleshckim@gmail.com>
2020-02-14 18:33:02 -0500

    Initial commit

---
+++ Sine_Wave_Gen.py
@@ -0,0 +1,28 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plot
+
+# Get x values of the sine wave
+time_index = np.arange(0, 65536, 1)
+# Amplitude of the sine wave is sine of a variable like time_index
+amplitude = np.sin(2 * math.pi * 655 / 65536 * time_index);
+
+#Plot a sine wave using time and amplitude obtained for the sine wave
+plot.plot(time_index, amplitude)
+
+#Give a title for the sine wave plot
+plot.title('Sine wave')
+
+#Give x axis label for the sine wave plot
+plot.xlabel('Time_index')
+
+# Give y axis label for the sine wave plot
+plot.ylabel('Amplitude=sin(time)')
+plot.axhline(y=0, color='k')
+plot.show()
+
+with open("output_data.csv","w") as out_file:
+    for i in range(len(amplitude)):
+        out_string = str(amplitude)
+        out_file.write(out_string)
+
